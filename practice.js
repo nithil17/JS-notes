95b82227d2bbb8390cd7cc1f27b1e02881279a9f
nithil17 <nithilnayan@yahoo.com>
2026-03-23 14:44:13 +0530

    added new notes to topic

--- practice.js
+++ practice.js
@@ -42,35 +42,66 @@ const { Input } = require("postcss");
 //   .then(() => uploadImage())
 //   .then(() => console.log("All steps completed"));
 
-var containsDuplicate = function (nums){
-  let map ={};
-  for(let num of nums){
-    if(map[num]){
-      return true;
-    }
-    map[num] = 1;
-  }
-  return false;
-};
+// var containsDuplicate = function (nums){
+//   let map ={};
+//   for(let num of nums){
+//     if(map[num]){
+//       return true;
+//     }
+//     map[num] = 1;
+//   }
+//   return false;
+// };
 
-let input =  [1,1,1,3];
+// let input =  [1,4,3,2, 7];
+// let target = 9;
 
 
-var unique = function(nums){
-  let map = {};
-  for(let num of nums){
-    map[num] = (map[num]|| 0) + 1;
+// var unique = function(nums){
+//   let map = {};
+//   for(let num of nums){
+//     map[num] = (map[num]|| 0) + 1;
 
-  }
+//   }
   
-  for (let num of nums){
-    if(map[num]===1){
-      return num;
-    }
+//   for (let num of nums){
+//     if(map[num]===1){
+//       return num;
+//     }
     
-  }
-  return -1;
-};
-console.log(unique(input));
+//   }
+//   return -1;
+// };
 
 
+// var double = function(nums, target){
+//   let map = {};
+//   for (let i = 0 ; i<nums.length; i++){
+//     let need = target - nums[i];
+//     if(map[need]!==undefined){
+//       return (map[need], i)
+//     }
+//      map[nums[i]] = i;
+//   }
+
+  
+// };
+
+// console.log(double(input, target));
+
+
+async function(){
+  let data =  await Axio.get("api");
+  let print = await JSON.data
+  console.log(print);
+
+}
+
+for(let item of allClasses){
+  item.style.cssText = `
+                          color:red;
+                          font-size:10px;
+                          border:2px solid brown;
+                          background-color: orange;
+                        `
+}
